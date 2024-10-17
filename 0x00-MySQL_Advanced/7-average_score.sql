e9cc2aac6284ed4ab4661d0e9fca3917a8c8e7d1
SeM2x <houssem135.302@gmail.com>
2024-10-17 10:29:42 +0100

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,17 @@
--- creates a stored procedure ComputeAverageScoreForUser that computes and store the average score for a student.
+-- creates a stored procedure ComputeAverageScoreForUser that computes and stores the average score for a student.
 DELIMITER //
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
 BEGIN
-    UPDATE users SET average_score = (SELECT SUM(score) / COUNT(score) FROM corrections WHERE user_id = user_id) WHERE id = user_id; 
+    UPDATE users 
+    SET average_score = (
+        SELECT SUM(score) / COUNT(score) 
+        FROM corrections 
+        WHERE corrections.user_id = user_id
+    ) 
+    WHERE id = user_id; 
 END;
 
 //
