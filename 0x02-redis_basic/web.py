d581bd20b60ce6644310aae5331908a4dfef4fa0
SeM2x <houssem135.302@gmail.com>
2024-10-23 16:12:32 +0100

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,44 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
+"""
+This module provides a decorator to count method calls
+using Redis and a function to fetch web page content.
+"""
 import requests
-from functools import wraps
+import redis
 from typing import Callable
+from functools import wraps
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+redis_instance = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def cach_data(method: Callable) -> Callable:
+    """
+    Decorator that counts the number of calls to a method using Redis.
+    """
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def inc(url: str) -> str:
+        """
+        Increments the value of a Redis key and then calls the given method.
+        """
+        count_key = f"count:{url}"
+        res_key = f"result:{url}"
+        expiration_time = 10
+        redis_instance.incr(count_key)
+        res = redis_instance.get(res_key)
+        if res:
+            return res.decode('utf-8')
+        res = method(url)
+        redis_instance.set(count_key, 0)
+        redis_instance.setex(res_key, expiration_time, res)
+        return res
+    return inc
 
 
-@data_cacher
+@cach_data
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """
+    Fetches the content of the given URL and returns it as a string.
+    """
+    response = requests.get(url)
+    return response.text
