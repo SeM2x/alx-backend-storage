3af7f15d949db3c57fc9dcd0c2d6ed9f52d20b73
SeM2x <houssem135.302@gmail.com>
2024-10-23 16:03:57 +0100

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module provides a decorator to count method calls
+using Redis and a function to fetch web page content.
+"""
 import requests
 import redis
 from typing import Callable, Any
 from functools import wraps
+
+redis_instance = redis.Redis()
 
 
 def count_calls(method: Callable) -> Callable:
@@ -14,11 +20,16 @@ def count_calls(method: Callable) -> Callable:
         """
         Increments the value of a Redis key and then calls the given method.
         """
-        key = f"count:{url}"
-        redis.Redis().incr(key)
+        count_key = f"count:{url}"
+        res_key = f"result:{url}"
         expiration_time = 10
-        redis.Redis().expire(key, expiration_time)
-        return method(url)
+        redis_instance.incr(count_key)
+        if redis_instance.exists(res_key):
+            return redis_instance.get(res_key).decode()
+        res = method(url)
+        redis_instance.set(res_key, res)
+        redis_instance.expire(res_key, expiration_time)
+        return res
     return inc
 
 
